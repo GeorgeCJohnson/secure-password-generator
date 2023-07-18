b4e0e9f31520676d4688e52d2a4fbd6738416489
George J <Gjohnson.pioneer@gmail.com>
2023-07-18 17:42:27 -0400

    Reformat js file for readability.

--- assets/script.js
+++ assets/script.js
@@ -2,46 +2,47 @@ var generateBtn = document.querySelector("#generate");
 
 //Declare characters to be used in password
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var special = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 var character = "";
-var passwordLength = "";
+var password = "";
 
-function generatePassword(){
+function generatePassword() {
   console.log("You clicked the button!"); 
   character = ""; 
   password = "";
+  var typecheck = false;
 
-//User prompts for the password criteria
+//User prompts for the password criteria every time a selection is made it is also confirmed.
 
 var characterLength = prompt("Please choose the number of character you want between 8 and 128."); 
 
 var length = Number(characterLength);
 
-  if (length = NaN || characterLength < 8 || characterLength > 128) {
+    if (length = NaN || characterLength < 8 || characterLength > 128) {
     alert("Enter a valid number of characters between 8 and 128.");
-    return password();
+    return "Please try again.";
     }
 
-  if (confirm("Do you want to include lowercase letters?")) {character = character.concat(lowerCase);alert("You have chosen to include lowercase letters.");}
-  else {alert("You have chosen not to include lowercase letters.");
-}
+    if (confirm("Do you want to include lowercase letters?")) {character = character.concat(lowercase);alert("You have chosen to include lowercase letters.");}
+    else {alert("You have chosen not to include lowercase letters.");
+    }
 
-  if (confirm("Do you want to include uppercase letters?")) {character = character.concat(upperCase); alert("You have chosen to include uppercase letters.");}
-  else {alert("You have chosen not to include uppercase letters.");
-}
+    if (confirm("Do you want to include uppercase letters?")) {character = character.concat(uppercase); alert("You have chosen to include uppercase letters.");}
+    else {alert("You have chosen not to include uppercase letters.");
+    }
 
-  if (confirm("Do you want to include numbers?")) { character = character.concat(numbers); alert("You have chosen to include numbers.");} 
-  else {alert("You have chosen not to include numbers.");
-}
+    if (confirm("Do you want to include numbers?")) { character = character.concat(numbers); alert("You have chosen to include numbers.");} 
+    else {alert("You have chosen not to include numbers.");
+    }
 
-  if (confirm("Do you want to include special characters?")) {character = character.concat(special); alert("You have chosen to include special characters.");}
-  else {alert("You have chosen not to include special characters.");
-}
+    if (confirm("Do you want to include special characters?")) {character = character.concat(special); alert("You have chosen to include special characters.");}
+    else {alert("You have chosen not to include special characters.");
+    }
 
-  for (var i = 0 ; i < characterLength; i++) {
+    for (var i = 0 ; i < characterLength; i++) {
     password += character[Math.floor(Math.random()*character.length)];
     }
     return generatePassword; 
